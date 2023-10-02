4d5029a54978554ad9f5f57639fc564c63dcfd18
Tarho <115691708+Tarho@users.noreply.github.com>
2023-10-02 18:46:27 +0700

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./MainStyle.css";
+
+function Main() {
+  return (
+    <div className='main-container'>
+      <p>Our Blog Start From Here</p>
+    </div>
+  )
+}
+
+export default Main
